a084a05f7f129a204168b06b763c9791ef54bd89
Edy Ruíz <edyrui2177@gmail.com>
2020-06-14 21:06:41 +0200

    Layout for profile added

--- app/src/main/java/com/ops/dev/simple/services/models/UsersModel.java
+++ app/src/main/java/com/ops/dev/simple/services/models/UsersModel.java
@@ -4,47 +4,37 @@ import java.io.Serializable;
 
 public class UsersModel  implements Serializable {
 
-   private String id;
-    private String dui;
+    private String id;
     private String name;
-    private String lastName;
     private String alias;
     private String phone;
     private String email;
     private String password;
-    private String birthDate;
-    private String gender;
+    private String picture;
     private String qrCode;
-    private String pictures;
     private String points;
     private String status;
     private String rol;
-    private String establishmentsFav;
+    private String businessFav;
     private String coupons;
-    private String idMembership;
 
     public UsersModel() {
     }
 
-    public UsersModel(String id, String dui, String name, String lastName, String alias, String phone, String email, String password, String birthDate, String gender, String qrCode, String pictures, String points, String status, String rol, String establishmentsFav, String coupons, String idMembership) {
+    public UsersModel(String id, String name, String alias, String phone, String email, String password, String qrCode, String picture, String points, String status, String rol, String businessFav, String coupons) {
         this.id = id;
-        this.dui = dui;
         this.name = name;
-        this.lastName = lastName;
         this.alias = alias;
         this.phone = phone;
         this.email = email;
         this.password = password;
-        this.birthDate = birthDate;
-        this.gender = gender;
         this.qrCode = qrCode;
-        this.pictures = pictures;
+        this.picture = picture;
         this.points = points;
         this.status = status;
         this.rol = rol;
-        this.establishmentsFav = establishmentsFav;
+        this.businessFav = businessFav;
         this.coupons = coupons;
-        this.idMembership = idMembership;
     }
 
     public String getId() {
@@ -55,28 +45,12 @@ public class UsersModel  implements Serializable {
         this.id = id;
     }
 
-    public String getDui() {
-        return dui;
-    }
-
-    public void setDui(String dui) {
-        this.dui = dui;
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getAlias() {
@@ -111,22 +85,6 @@ public class UsersModel  implements Serializable {
         this.password = password;
     }
 
-    public String getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(String birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
     public String getQrCode() {
         return qrCode;
     }
@@ -135,12 +93,12 @@ public class UsersModel  implements Serializable {
         this.qrCode = qrCode;
     }
 
-    public String getPictures() {
-        return pictures;
+    public String getPicture() {
+        return picture;
     }
 
-    public void setPictures(String pictures) {
-        this.pictures = pictures;
+    public void setPicture(String pictures) {
+        this.picture = pictures;
     }
 
     public String getPoints() {
@@ -167,12 +125,12 @@ public class UsersModel  implements Serializable {
         this.rol = rol;
     }
 
-    public String getEstablishmentsFav() {
-        return establishmentsFav;
+    public String getBusinessFav() {
+        return businessFav;
     }
 
-    public void setEstablishmentsFav(String establishmentsFav) {
-        this.establishmentsFav = establishmentsFav;
+    public void setBusinessFav(String businessFav) {
+        this.businessFav = businessFav;
     }
 
     public String getCoupons() {
@@ -181,13 +139,5 @@ public class UsersModel  implements Serializable {
 
     public void setCoupons(String coupons) {
         this.coupons = coupons;
-    }
-
-    public String getIdMembership() {
-        return idMembership;
-    }
-
-    public void setIdMembership(String idMembership) {
-        this.idMembership = idMembership;
     }
 }
